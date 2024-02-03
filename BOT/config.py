bbe691fd79b35e51f563e106399c9765b2a74752
ibobrovs <153681447+ibobrovs@users.noreply.github.com>
2024-02-03 19:39:42 +0200

    Add files via upload

---
+++ BOT/config.py
@@ -0,0 +1,7 @@
+TOKEN = '6869641752:AAGmIDWtL62JjU8pnObcQqdG0I9HdWmFnn0'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
---
+++ BOT/extensions.py
@@ -0,0 +1,8 @@
+import telebot
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
